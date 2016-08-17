1268538e60accc77b7e714f85a3fac421d3f6769
ovrmrw <t.ngc.0403@gmail.com>
2016-08-17 17:38:59 +0900

    Fix

--- server.ts
+++ server.ts
@@ -1,124 +1,8 @@
 import 'babel-polyfill'
-import lodash from 'lodash'
 
-require('dotenv').load()
+import { CloudantController, DocumentBase, SearchResult } from './cloudant-controller'
 
-const account: string = process.env.CLOUDANT_USERNAME || ''
-const key: string = process.env.CLOUDANT_API_KEY || ''
-const password: string = process.env.CLOUDANT_API_PASSWORD || ''
-
-
-class CloudantController {
-  cloudant: any;
-
-  constructor(account: string, key: string, password: string) {
-    const Cloudant = require('cloudant')
-    this.cloudant = Cloudant({ account, key, password })
-  }
-
-  // NEEDS _admin permission
-  createDatabase(dbName: string): Promise<{} | null> {
-    return new Promise<{} | null>((resolve, reject) => {
-      this.cloudant.db.create(dbName, (err, body, header) => {
-        if (err) {
-          console.error(err)
-          reject(null)
-          return
-        }
-        console.log('Create Database')
-        console.log(body)
-        resolve(body)
-      })
-    })
-  }
-
-  // NEEDS _admin permission
-  dropDatabase(dbName: string): Promise<{} | null> {
-    return new Promise<{} | null>((resolve, reject) => {
-      this.cloudant.db.destroy(dbName, (err, body, header) => {
-        if (err) {
-          console.error(err)
-          reject(null)
-          return
-        }
-        console.log('Destroy Database')
-        console.log(body)
-        resolve(body)
-      })
-    })
-  }
-
-  private use(dbName: string): any {
-    return this.cloudant.db.use(dbName)
-  }
-
-  insertDocument<T>(dbName: string, insertObj: T, _id?: string): Promise<{} | null> {
-    return new Promise<{} | null>((resolve, reject) => {
-      const db = this.use(dbName)
-      const temp = lodash.defaultsDeep<T, T>(insertObj, _id ? { _id: _id } : {})
-      db.insert(temp, (err, body, header) => {
-        if (err) {
-          console.error(err)
-          reject(null)
-          return
-        }
-        console.log('Insert Document')
-        console.log(body)
-        resolve(body)
-      })
-    })
-  }
-
-  getDocument<T>(dbName: string, _id: string): Promise<T | null> {
-    return new Promise<T | null>((resolve, reject) => {
-      const db = this.use(dbName)
-      db.get(_id, (err, data, header) => {
-        if (err) {
-          console.error(err)
-          reject(null)
-          return
-        }
-        console.log('Get Document')
-        console.log(data)
-        resolve(data)
-      })
-    })
-  }
-
-  deleteDocument(dbName: string, deleteObj: DocumentBase): Promise<{} | null> {
-    return new Promise<{} | null>((resolve, reject) => {
-      const db = this.use(dbName)
-      db.destroy(deleteObj._id, deleteObj._rev, (err, body, header) => {
-        if (err) {
-          console.error(err)
-          reject(null)
-          return
-        }
-        console.log('Delete Document')
-        console.log(body)
-        resolve(body)
-      })
-    })
-  }
-
-  searchDocument<T>(dbName: string, designName: string, indexName: string, searchText: string): Promise<T | null> {
-    return new Promise<T | null>((resolve, reject) => {
-      const db = this.use(dbName)
-      db.search(designName, indexName, { q: searchText }, (err, result, header) => {
-        if (err) {
-          console.error(err)
-          reject(null)
-          return
-        }
-        console.log('Search Document')
-        console.log(result)
-        resolve(result)
-      })
-    })
-  }
-
-}
-
+const cc = new CloudantController();
 
 // cloudant.db.destroy('alice', (err) => {
 //   if (err) { console.error(err) }
@@ -138,7 +22,6 @@ class CloudantController {
 //////////////////////////////////////////////////////////////////////////////
 
 // const DB = 'alice' // Database Name which you created.
-const cc = new CloudantController(account, key, password);
 
 /*
 (async () => {
@@ -158,11 +41,6 @@ const cc = new CloudantController(account, key, password);
   }
 })()
 */
-
-interface DocumentBase {
-  _id?: string
-  _rev?: string
-}
 
 interface Alice {
   crazy: boolean
@@ -188,16 +66,4 @@ const DB = 'mydb' // Database Name which you created.
 interface MyDoc {
   name: string
   desc: string
-}
-
-interface SearchResult<T> {
-  total_rows: number
-  bookmark: string
-  rows: SearchResultRow<T>[]
-}
-
-interface SearchResultRow<T> {
-  id: string
-  order: number[]
-  fields: T
 }
